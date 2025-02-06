6bdcc378bc3f9ab4409926eeb3ecd426bdce19cf
johnangel05 <137510405+johnangel05@users.noreply.github.com>
2025-02-06 13:14:28 +0800

    STYLESHEET

---
+++ activity-2/activity-2/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+      <Text style={styles.boldText}>John Angel</Text>
+      <Text style={styles.italicText}>Delos Reyes</Text>
+      </Text>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#eaeaea',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  italicText: {
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
